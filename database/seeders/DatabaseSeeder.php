1ed289ce35918c3dc9e91d4b0e638401c10030f5
Ethan Zitting <ethan@ethanzitting.com>
2023-04-02 10:37:29 -0500

    Establish first database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory()
+            ->hasPosts()
+            ->hasProfile()
+            ->create([
+                'email' => 'dev@robotriffs.com',
+                'name' => 'Ethan Zitting',
+                'handle' => 'ethanzitting',
+                'password' => Hash::make('admin'),
+            ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(10)
+            ->hasProfile()
+            ->hasPosts(3)
+            ->create();
     }
 }
