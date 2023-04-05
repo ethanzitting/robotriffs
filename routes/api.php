fcce9d5e73443e7e613633b8c0511e20e5dca2c7
Ethan Zitting <ethan@ethanzitting.com>
2023-04-04 20:51:07 -0500

    Add Spatie Query Builder to project with examples

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\FollowController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,4 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::resource('/follows', FollowController::class);
+Route::resources([
+    'follows' => \App\Http\Controllers\FollowController::class,
+    'users' => \App\Http\Controllers\UserController::class
+]);
