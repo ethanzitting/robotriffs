279e10d2d3f67dac348a91a55ccf1e11e1dad423
Ethan Zitting <ethan@ethanzitting.com>
2023-06-06 20:32:56 -0500

    Refresh feed on tweet submission

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,10 +2,13 @@
 import DefaultLayout from '@/Layouts/DefaultLayout.vue';
 import TweetInput from "@/Components/tweets/TweetInput.vue";
 import UserFeed from "../Components/tweets/UserFeed.vue";
+import {ref} from "vue";
 
-defineProps({
-    user: Object,
-})
+defineProps({ user: Object })
+
+let feedKey = ref(0)
+
+const reloadFeed = () => feedKey = feedKey.value + 1
 </script>
 
 <template>
@@ -15,8 +18,12 @@ defineProps({
         </template>
 
         <div class="main">
-            <TweetInput placeholder="What's Happening?" :user="user.data"/>
-            <suspense>
+            <TweetInput
+                placeholder="What's Happening?"
+                :user="user.data"
+                @submitted="reloadFeed"
+            />
+            <suspense :key="feedKey">
                 <UserFeed :user="user.data"/>
             </suspense>
         </div>
