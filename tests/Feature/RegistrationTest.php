144a550e82f9d7b6b6a8752a9e4b5aca25e7158f
Ethan Zitting <ethan@ethanzitting.com>
2023-06-10 19:46:45 -0500

    Fix broken password confirmation test

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -40,12 +40,6 @@ class RegistrationTest extends TestCase
 
     public function test_new_users_can_register(): void
     {
-        if (! Features::enabled(Features::registration())) {
-            $this->markTestSkipped('Registration support is not enabled.');
-
-            return;
-        }
-
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
