094cde895f03aaa6d17e8985fd0bebfc4fa5feac
Ethan Zitting <ethan@ethanzitting.com>
2023-06-13 14:12:18 -0500

    Add todo steps for better PWA ux

--- resources/js/Components/PWAInstallButton.vue
+++ resources/js/Components/PWAInstallButton.vue
@@ -3,6 +3,14 @@ import IconDownload from "./icons/IconDownload.vue";
 import {systemStore} from "../stores/system";
 
 const system = systemStore();
+
+// TODO: Only show button on mobile devices.
+
+// TODO: Download on android, show guide modal on IOS.
+
+// TODO: Make sure the button never shows if we are in standalone mode.
+
+// TODO: Use a cookie to determine if the user has installed the app previously.
 </script>
 
 <template>
