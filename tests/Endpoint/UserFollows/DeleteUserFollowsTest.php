3d499237dc93727ce7bd67949ad3f5da0e2d45d9
Ethan Zitting <ethan@ethanzitting.com>
2023-06-21 18:50:22 -0500

    Scaffold out DeleteUserFollowsTest.php

--- tests/Endpoint/UserFollows/DeleteUserFollowsTest.php
+++ tests/Endpoint/UserFollows/DeleteUserFollowsTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Endpoint\UserFollows;
 
+use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Testing\TestResponse;
 use Tests\TestCase;
 use Tests\Traits\GuestAccessForbidden;
@@ -10,13 +12,32 @@ class DeleteUserFollowsTest extends TestCase
 {
     use GuestAccessForbidden;
 
-    public function testDestroysResource()
+    protected function setUp(): void
     {
-        $this->fail();
+        parent::setUp();
+
+        $user = User::factory()
+            ->hasFollowers()
+            ->create();
+
+        $this->follow = DB::table('follows')
+            ->where('follower_id', $user->followers->first()->id)
+            ->where('followed_id', $user->id)
+            ->first();
     }
 
-    public function submitRequest(): TestResponse
+    public function testDestroysResource()
     {
-        return $this->deleteJson('/api/user-follows');
+        $this->authenticate();
+
+        $this->submitRequest($this->follow->followed_id)
+            ->assertStatus(204);
+
+        $this->assertDatabaseMissing('follows', $this->follow->toArray());
+    }
+
+    public function submitRequest(?int $followedId = null): TestResponse
+    {
+        return $this->deleteJson('/api/user-follows?followed='.$followedId);
     }
 }
