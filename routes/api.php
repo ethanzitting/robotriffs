0141d61255e9cd86253bcee9d9c72d9a069f0c88
Ethan Zitting <ethan@ethanzitting.com>
2024-05-31 19:21:57 -0500

    Expand guest visibility of the site

--- routes/api.php
+++ routes/api.php
@@ -19,14 +19,21 @@ Route::middleware('auth:sanctum')
         return $request->user();
     });
 
+Route::get('tweets', [\App\Http\Controllers\API\TweetController::class, 'index']);
+Route::get('tweets/{tweet}/replies', [\App\Http\Controllers\API\TweetReplyController::class, 'index']);
+Route::get('user-follows', [\App\Http\Controllers\API\UserFollowController::class, 'index']);
+Route::get('users', [\App\Http\Controllers\API\UserController::class, 'index']);
+Route::get('users/{user}', [\App\Http\Controllers\API\UserController::class, 'show']);
+
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('tweets', [\App\Http\Controllers\API\TweetController::class, 'store']);
+    Route::delete('tweets/{tweet}', [\App\Http\Controllers\API\TweetController::class, 'destroy']);
+    Route::post('user-follows', [\App\Http\Controllers\API\UserFollowController::class, 'store']);
+    Route::delete('user-follows/{followed}', [\App\Http\Controllers\API\UserFollowController::class, 'destroy']);
+
     Route::apiResources([
-        'users' => \App\Http\Controllers\API\UserController::class,
-        'user-follows' => \App\Http\Controllers\API\UserFollowController::class,
         'likes' => \App\Http\Controllers\API\LikeController::class,
-        'tweets' => \App\Http\Controllers\API\TweetController::class,
         'images' => \App\Http\Controllers\API\ImageController::class,
-        'tweets.replies' => \App\Http\Controllers\API\TweetReplyController::class,
         'feeds' => \App\Http\Controllers\API\FeedController::class,
         'notifications' => \App\Http\Controllers\API\NotificationController::class,
     ]);
